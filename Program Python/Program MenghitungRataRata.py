6ee63f6be9246dfa76e38acc5520fd6f846764e8
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-11 12:04:15 +0700

    Add files via upload

---
+++ Program Python/Program MenghitungRataRata.py
@@ -0,0 +1,13 @@
+#SOAL Repl.it DDP pekan 1 dan 2
+# SOAL 1 - Menghitung rata-rata
+# Tuliskan program untuk Soal 1 di bawah ini
+print ("PROGRAM PYTHON MENGHITUNG Rata Rata")
+print ("Program ini dibuat oleh Alif Luthan")
+x = int(input("Masukan Nilai X = "))
+y = int(input("Masukan Nilai y  = "))
+z = int(input("Masukan Nilai Z = "))
+
+v = ( x+y+z ) / 3
+
+print ("Hasil Perhitungan = %i" %v)
+
