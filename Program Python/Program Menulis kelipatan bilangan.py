b237b5c24334bd75c7763f3dfe6c356da19b90bc
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-11 12:08:38 +0700

    Add files via upload

---
+++ Program Python/Program Menulis kelipatan bilangan.py
@@ -0,0 +1,8 @@
+# Menulis kelipatan bilangan
+# Program menghitung Kelipatan dan dipisah menggunakan " - "
+
+angka = eval(input("masukan angka dari :"))
+
+while angka < 100:
+    print(angka , end=" --- ")
+    angka = angka + angka
