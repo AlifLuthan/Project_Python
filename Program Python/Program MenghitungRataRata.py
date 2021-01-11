3297c99a179cfd5a81e4530713fc1e76fd9be9b1
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-11 12:05:52 +0700

    Update Program MenghitungRataRata.py
    beberapa fix yang harus diluruskan agar tidak salah paham

--- Program Python/Program MenghitungRataRata.py
+++ Program Python/Program MenghitungRataRata.py
@@ -1,6 +1,4 @@
-#SOAL Repl.it DDP pekan 1 dan 2
-# SOAL 1 - Menghitung rata-rata
-# Tuliskan program untuk Soal 1 di bawah ini
+# Menghitung rata-rata
 print ("PROGRAM PYTHON MENGHITUNG Rata Rata")
 print ("Program ini dibuat oleh Alif Luthan")
 x = int(input("Masukan Nilai X = "))
