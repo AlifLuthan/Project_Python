5e9a9ecf301ed7ccde35411c55cb95091c936b8e
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-12 15:08:29 +0700

    Add files via upload

---
+++ Program Python II/Program Menghitung rata-rata.py
@@ -0,0 +1,14 @@
+# Menghitung rata-rata
+# Program Dibawah ini dibuat untuk Menghitung Rata Rata
+
+x =int(input("Masukan Angka :"))
+y =int(input("Masukan Angka :"))
+z =int(input("Masukan Angka :"))
+
+a = ( x+y+z ) / 5
+print("hasil perhitungannya : %i" %a)
+
+# Program yang saya buat menggunakan tipe data integer(%i)
+# saat semua inputan x,y,z dan a itu di eksekusi
+# maka nanti akan terlihat hasil dari hitung rata rata
+# yang telah di bagi 5
