f13845f129b3fe15b51cf9ab90015f0753965bd9
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-12 15:02:22 +0700

    Add files via upload

---
+++ Program Python II/Program Mencetak bilangan.py
@@ -0,0 +1,10 @@
+# Mencetak bilangan
+# Gunakan perulangan FOR untuk mengelolah bilangan.
+
+for angka in range(2, 102, 2):
+    print(angka , end=",")
+
+
+# Program yang saya buat ini mengulang angka dari 2
+# sampai dengan 100 dengan menggunakan for dengan
+# menggunakan 3 parameter (awalan , akhiran , selang)
