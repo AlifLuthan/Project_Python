aedf33c99433f8ec94a2d7e8c25794f321fbcfdd
AlifLuthan <55443868+AlifLuthan@users.noreply.github.com>
2021-01-14 12:34:39 +0700

    Add files via upload

---
+++ Program Python III/Program Membuat sebuah Persegi.py
@@ -0,0 +1,10 @@
+# Mencetak persegi
+# Program Dibawah Output Cetakannya akan Membuat Persegi.
+
+for i in range(4):
+    print("5#" * 4)
+
+# di program ini saya memakai range 4 dan * 5 itu maksudnya
+# agar program saya nanti berjalan ke bawah itu 4
+# dan menyamping dengan 4 baris yang isinya 6#
+# dan membetuk sebuah persegi 
