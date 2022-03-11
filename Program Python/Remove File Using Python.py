15730a87354c4c7b237764cd421161b181c1ad1f
Mas Alip <55443868+AlifLuthan@users.noreply.github.com>
2022-03-11 15:25:56 +0700

    Update Remove File Using Python.py

--- Program Python/Remove File Using Python.py
+++ Program Python/Remove File Using Python.py
@@ -1,15 +1,13 @@
-# Referensi : https://careerkarma.com/blog/python-delete-file/
-# Referensi : https://www.w3schools.com/python/python_file_remove.asp
-# Referensi : https://stackoverflow.com/questions/6996603/how-to-delete-a-file-or-folder-in-python
-# Referensi : https://www.geeksforgeeks.org/delete-a-directory-or-file-using-python/
 # Referensi : https://www.geeksforgeeks.org/os-module-python-examples/
 
 
 import os
+import sys
 # apa fungsi dari module OS ? Module OS itu menyediakan Fungsi untuk Menghapus sebuah Direktori(Folder)
 #selain Menghapus , module OS juga dapat mengubah dan Mengindentifikasi direktori(folder)
 
-path = input("Path File : ")# Input Path / Nama File yang ingin dihapus
+path = sys.argv[1] # Ditambahkan CLA agar Behavior lebih sama persis.
+#path = input("Path File : ")# Input Path / Nama File yang ingin dihapus
 if os.path.exists(path): # Jika didalam Path terdapat sebuah File
     os.remove(path) # Hapus File tersebut
     print("File Kamu Berhasil Terhapus : " , path) # Jika berhasil terhapus hasilnya Berhasil
